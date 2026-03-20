735c8751d248ce843acb32319221217de5fb5992
nnekit10 <nnekit10@yandex.ru>
2026-03-20 09:31:17 +0300

    First com

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSentData() {
+        String requestBody = "some data";
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(requestBody)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(requestBody));
+    }
+}
